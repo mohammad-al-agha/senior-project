005c3d5008a85906e1a85c779077e3816863b587
mohammad-al-agha <mohalagha11@gmail.com>
2024-07-25 20:42:12 +0300

    Connected the app to the data base

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.config";
 
 const app: Express = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running on Port ${process.env.PORT}`);
-});
+app
+  .listen(process.env.PORT, () => {
+    console.log(`Server Running on Port ${process.env.PORT}`);
+    connectDB();
+  })
+  .on("error", (e) => {
+    console.log(e);
+  });
