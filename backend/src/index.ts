09b3063b9d5680266fe2f6050b0d41a11f5a9219
mohammad-al-agha <mohalagha11@gmail.com>
2024-07-29 21:46:11 +0300

    Added routes to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,26 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.config";
+import AuthRouter from "./routes/auth.routes";
+import CourseRouter from "./routes/course.routes";
+import StudentRouter from "./routes/student.routes";
+import InstructorRouter from "./routes/instructor.routes";
 
 const app: Express = express();
+
+app.use(express.json());
+
+//auth
+app.use("/auth", AuthRouter);
+
+//course
+app.use("/course", CourseRouter);
+
+//instructor
+app.use("/instructor", InstructorRouter);
+
+//student
+app.use("/student", StudentRouter);
 
 app
   .listen(process.env.PORT, () => {
