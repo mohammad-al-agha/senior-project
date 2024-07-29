c5e86e9f9e0cd2af84f06821a4ed2b669b8a0e9c
mohammad-al-agha <mohalagha11@gmail.com>
2024-07-29 21:50:24 +0300

    Created the auth controller

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,141 @@
+import { Request, Response } from "express";
+import { LoginAuthDTO, RegisterAuthDTO } from "./dtos/auth.dto";
+import { Student } from "../models/student.model";
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { Instructor } from "../models/instructor.model";
+import { validateEmail, validatePassword } from "../utilities/regex";
+
+//Login as a student
+
+export const loginAsStudent = async (
+  req: Request<{}, {}, LoginAuthDTO>,
+  res: Response
+) => {
+  const { email, password } = req.body;
+
+  const student = await Student.findOne({ studentEmail: email });
+
+  if (!student) {
+    return res.status(409).json({ message: "Invalid Credentials" });
+  }
+
+  validatePassword(password);
+
+  const compare = await bcrypt.compare(password, student.studentPassword);
+
+  if (!compare) {
+    return res.status(409).json({ message: "Invalid Credentials" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: student._id,
+      email: student.studentEmail,
+    },
+    process.env.JWT_SECRET
+  );
+
+  return res.json({ token: token, student: student });
+};
+
+//Login as an instructor
+
+export const loginAsInstructor = async (
+  req: Request<{}, {}, LoginAuthDTO>,
+  res: Response
+) => {
+  const { email, password } = req.body;
+
+  const instructor = await Instructor.findOne({ instructorEmail: email });
+
+  if (!instructor) {
+    return res.status(409).json({ message: "Invalid Credentials" });
+  }
+
+  const compare = await bcrypt.compare(password, instructor.instructorPassword);
+
+  if (!compare) {
+    return res.status(409).json({ message: "Invalid Credentials pass" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: instructor._id,
+      email: instructor.instructorEmail,
+    },
+    process.env.JWT_SECRET
+  );
+
+  return res.json({ token: token, instructor: instructor });
+};
+
+//Sign Up
+
+export const register = async (
+  req: Request<{}, {}, RegisterAuthDTO>,
+  res: Response
+) => {
+  const { name, email, password, type } = req.body;
+
+  if (!validateEmail(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  if (!validatePassword(password)) {
+    return res.status(400).json({
+      message:
+        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number",
+    });
+  }
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  if (type === "Student") {
+    const student = await Student.findOne({ studentEmail: email });
+
+    if (student) {
+      return res.status(409).json({ message: "The User already registered" });
+    }
+
+    const newStudent = new Student({
+      studentName: name,
+      studentEmail: email,
+      studentPassword: hashedPass,
+    });
+
+    await newStudent.save();
+
+    const token = jwt.sign(
+      { id: newStudent._id, email: newStudent.studentEmail },
+      process.env.JWT_SECRET
+    );
+
+    return res.json({ token: token, student: newStudent });
+  } else {
+    const instructor = await Instructor.findOne({ instructorEmail: email });
+
+    if (instructor) {
+      return res
+        .status(409)
+        .json({ message: "The instructor is already registered" });
+    }
+
+    const newinstructor = new Instructor({
+      instructorName: name,
+      instructorEmail: email,
+      instructorPassword: hashedPass,
+    });
+
+    newinstructor.save();
+
+    const token = jwt.sign(
+      { id: newinstructor._id, email: newinstructor.instructorEmail },
+      process.env.JWT_SECRET
+    );
+
+    return res.json({ token: token, instructor: newinstructor });
+  }
+};
