cffd0768175125b8b9eb5f3604ef14f19c9c560b
mohammad-al-agha <mohalagha11@gmail.com>
2024-07-31 20:24:33 +0300

    Read the theme in the main app for dark mode

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import "./index.css";
 import Login from "./pages/auth/login/Login";
+import { RootState } from "./redux/store";
+import { ThemeType } from "./core/types/themeTypes";
 
 function App() {
+  const isDark = useSelector((state: RootState) => state.theme.currentTheme);
+
   return (
-    <>
+    <div dark-theme={isDark === ThemeType.dark ? "dark" : "light"}>
       <Login />
-    </>
+    </div>
   );
 }
 
