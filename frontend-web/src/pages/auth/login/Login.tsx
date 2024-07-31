605457458b7543fa9551c7ccae662b1c770b1ae7
mohammad-al-agha <mohalagha11@gmail.com>
2024-07-31 19:32:54 +0300

    Edited the login screen structure

--- frontend-web/src/pages/auth/login/Login.tsx
+++ frontend-web/src/pages/auth/login/Login.tsx
@@ -1,13 +1,57 @@
 import "./Login.css";
+import LogoBright from "../../../../assets/images/LogoBright.svg";
+import Board from "../../../../assets/images/Board.svg";
+import Rocket from "../../../../assets/images/Rocket.svg";
+import Bulb from "../../../../assets/images/Bulb.svg";
+import Brain from "../../../../assets/images/Brain.svg";
+import Atom from "../../../../assets/images/Atom.svg";
+import Molecule from "../../../../assets/images/Molecule.svg";
+import Hero from "../../../../assets/images/Hero.svg";
+import UserType from "./login-components/UserType";
+import Student from "../../../../assets/images/Student.svg";
+import Instructor from "../../../../assets/images/Instructor.svg";
 
 const Login = () => {
   return (
     <div className="login-body">
-      <h1>Synergy</h1>
-      <div className="login-wrapper">
-        <h1>Welcome To Synergy!</h1>
-        <div className="input-section">
-          <label htmlFor="Email">EMAIL</label>
+      <div className="login-body--left">
+        <section className="login-icon login-icon--board">
+          <img src={Board} height={65} width={155} alt="" />
+        </section>
+        <section className="login-icon login-icon--atom">
+          <img src={Atom} height={65} width={155} alt="" />
+        </section>
+        <section className="login-icon login-icon--brain">
+          <img src={Brain} height={65} width={155} alt="" />
+        </section>
+        <section className="login-icon login-icon--molecule">
+          <img src={Molecule} height={65} width={155} alt="" />
+        </section>
+        <section className="login-icon login-icon--rocket">
+          <img src={Rocket} height={65} width={155} alt="" />
+        </section>
+        <section className="login-icon login-icon--bulb">
+          <img src={Bulb} height={65} width={155} alt="" />
+        </section>
+        <h1>Welcome To Synergy !</h1>
+        <img src={Hero} height={500} width={800} alt="" />
+      </div>
+      <div className="login-body--right">
+        <img src={LogoBright} height={125} width={225} alt="" />
+        {/* <div className="input-section--checkbox">
+          <label htmlFor="userType">Student</label>
+          <input type="checkbox" id="userType" />
+        </div>
+        <div className="input-section--checkbox">
+          <label htmlFor="userType">Instructor</label>
+          <input type="checkbox" id="userType" />
+        </div> */}
+        <div className="user-types">
+          <UserType icon={Student} type="Student" value="Std" />
+          <UserType icon={Instructor} type="Instructor" value="Inst" />
+        </div>
+        <div className="input-section--text">
+          <label htmlFor="Email">Email</label>
           <input
             type="email"
             id="Email"
@@ -15,8 +59,8 @@ const Login = () => {
             placeholder="example@gmail.com"
           />
         </div>
-        <div className="input-section">
-          <label htmlFor="Pass">PASSWORD</label>
+        <div className="input-section--text">
+          <label htmlFor="Pass">Password</label>
           <input
             type="password"
             id="Pass"
