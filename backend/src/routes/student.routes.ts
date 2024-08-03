7da95c0647525d6b5383314740f5c772a94daab7
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-03 23:55:41 +0300

    Added all the student routes

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { loginAsStudent } from "../controllers/auth.controller";
+import {
+  getProgress,
+  getStudentCourses,
+  setTargets,
+} from "../controllers/student.controller";
 
 const router = Router();
 
-router.post("/login", loginAsStudent);
+router.get("/getCourses", getStudentCourses);
+router.post("/setTargets", setTargets);
+router.get("/getProgress", getProgress);
 
 export default router;
