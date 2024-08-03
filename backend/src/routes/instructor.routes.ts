6e44277f696fc54592785bf30bacb254020a16ee
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-03 23:55:29 +0300

    Added all the intructor routes

--- backend/src/routes/instructor.routes.ts
+++ backend/src/routes/instructor.routes.ts
@@ -1,4 +1,11 @@
 import { Router } from "express";
+import {
+  addCommentOnTargets,
+  getInstructorCourses,
+} from "../controllers/instructor..controller";
 const router = Router();
+
+router.get("/getInstructorCourses", getInstructorCourses);
+router.post("/addCommentOnTargets", addCommentOnTargets);
 
 export default router;
