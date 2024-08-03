6dd83b6a4d8a504bb81e75d55dc4bf6d7f7fae73
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-03 22:36:28 +0300

    Intalled and used the react router dom package to handle navigation & routing

--- frontend-web/src/main.tsx
+++ frontend-web/src/main.tsx
@@ -1,11 +1,24 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/auth/login/Login.tsx";
+import Home from "./pages/Home/Home.tsx";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={Router} />
   </Provider>
 );
