472c66e0da64958084c84717522533db60f69c52
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-04 17:47:23 +0300

    Fixed the instructor routes

--- backend/src/routes/instructor.routes.ts
+++ backend/src/routes/instructor.routes.ts
@@ -5,7 +5,7 @@ import {
 } from "../controllers/instructor..controller";
 const router = Router();
 
-router.get("/getInstructorCourses", getInstructorCourses);
+router.get("/getCourses", getInstructorCourses);
 router.post("/addCommentOnTargets", addCommentOnTargets);
 
 export default router;
