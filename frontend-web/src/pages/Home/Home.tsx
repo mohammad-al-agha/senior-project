9a90a72c987941f2e4ce5389c7c6a35284525550
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-10 14:29:41 +0300

    Edited the home page

--- frontend-web/src/pages/Home/Home.tsx
+++ frontend-web/src/pages/Home/Home.tsx
@@ -1,37 +1,12 @@
-import axios from "axios";
 import SideBar from "./Home Components/Side Bar/SideBar";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../redux/store";
-import { setCourses } from "../../redux/courses";
+
 import "./HomePage.css";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import CourseCardWrapper from "./Home Components/Course Cards/CourseCardWrapper";
+import { useHomeLogic } from "./useHomeLogic";
 
 const Home = () => {
-  const user = useSelector((state: RootState) => state.user.userType);
-
-  const dispatch = useDispatch();
-
-  const token = localStorage.getItem("token");
-
-  const location = useLocation();
-
-  const getCourses = {
-    method: "get",
-    url: `http://localhost:8000/${user}/getCourses`,
-    headers: {
-      "content-type": "application/json",
-      Accept: "application/json",
-      Authorization: "bearer " + token,
-    },
-  };
-
-  useEffect(() => {
-    axios.request(getCourses).then((res) => {
-      dispatch(setCourses(res.data));
-    });
-  }, []);
+  const { location } = useHomeLogic();
 
   return (
     <div className="home-page">
