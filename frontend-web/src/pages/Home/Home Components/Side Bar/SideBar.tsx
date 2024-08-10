21a32e3555c2ba1a6e12ad523e848d8c1b714cb4
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-10 14:30:11 +0300

    Fixed naming null name on reload

--- frontend-web/src/pages/Home/Home Components/Side Bar/SideBar.tsx
+++ frontend-web/src/pages/Home/Home Components/Side Bar/SideBar.tsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import { Course } from "../../../../redux/courses/coursesSlice";
 import { RootState } from "../../../../redux/store";
-import { useEffect } from "react";
 import SideBarButton from "./Side Bar Components/SideBarButton";
 import "./SideBar.css";
 import SideBarOptionButton from "./Side Bar Components/SideBarOptionButton";
@@ -10,15 +9,14 @@ import Gear from "../../../../../assets/images/Gear.svg";
 
 const SideBar = () => {
   const courses = useSelector((state: RootState) => state.course.courses);
-
+  const userName = localStorage.getItem("name");
   const user = useSelector((state: RootState) => state.user);
 
-  useEffect(() => {}, []);
   return (
     <div className="side-bar">
       <div className="side-bar-upper">
         {/* <img src="" alt="student photo" /> */}
-        <h2>{user.userName}</h2>
+        <h2>{user.userName ? user.userName : userName}</h2>
       </div>
       <div className="side-bar-classes">
         {courses.map((course: Course) => {
