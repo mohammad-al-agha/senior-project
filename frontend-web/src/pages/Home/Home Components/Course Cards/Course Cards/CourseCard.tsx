0f0e1e4fce84137dbec6707571b49f696e893fa8
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-15 22:32:57 +0300

    Edited the styling of the course cards

--- frontend-web/src/pages/Home/Home Components/Course Cards/Course Cards/CourseCard.tsx
+++ frontend-web/src/pages/Home/Home Components/Course Cards/Course Cards/CourseCard.tsx
@@ -1,11 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { setCurrentCourse } from "../../../../../redux/course";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Course } from "../../../../../redux/courses/coursesSlice";
 import "./CourseCard.css";
+import { RootState } from "../../../../../redux/store";
+import LinkW from "../../../../../../assets/images/LinkW.svg";
+import LinkB from "../../../../../../assets/images/LinkB.svg";
+import ClockW from "../../../../../../assets/images/ClockW.svg";
+import ClockB from "../../../../../../assets/images/ClockB.svg";
+import { useBackgroundIcon } from "../../../../../hooks/useBackgroundIcon";
 
 const CourseCard = ({ course }: { course: Course }) => {
   const dispatch = useDispatch();
+  const isDark = useSelector((state: RootState) => state.theme.currentTheme);
+  const icon = useBackgroundIcon(course.icon, isDark);
 
   const handleCourseChange = () => {
     dispatch(setCurrentCourse(course));
@@ -17,10 +25,21 @@ const CourseCard = ({ course }: { course: Course }) => {
       className="course-card"
       onClick={() => handleCourseChange()}
     >
-      <h3 className="course-card-title">{course.courseName}</h3>
-      <div className="course-card-body">
-        <h4>Course Code: {course.courseCode}</h4>
-        <h4>Meetink Link: {course.courseMeetingLink}</h4>
+      <div className="course-card-title">
+        <h2>{course.courseName}</h2>
+        <h3>{course.courseCode}</h3>
+      </div>
+      <div className={`course-card-body ${icon}`}>
+        <div className="course-details">
+          <img src={isDark ? LinkW : LinkB} alt="" />
+          <h4>Meetink Link: {course.courseMeetingLink}</h4>
+        </div>
+        <div className="course-details">
+          <img src={isDark ? ClockW : ClockB} alt="" />
+          <h4>
+            Sessions each {course.sessionDate} at {course.time}
+          </h4>
+        </div>
       </div>
     </NavLink>
   );
