0aa39357f1f4fe2ea22447bda298c30cd70563fb
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-15 22:31:21 +0300

    Edited the feed structure and styling

--- frontend-web/src/pages/Home/Home Components/Home Feed/HomeFeed.tsx
+++ frontend-web/src/pages/Home/Home Components/Home Feed/HomeFeed.tsx
@@ -1,19 +1,40 @@
 import "./HomeFeed.css";
 import { useHomeFeedLogic } from "./useHomeFeedLogic";
+import Upload from "../../../../../assets/images/Upload.svg";
+import EyeW from "../../../../../assets/images/EyeW.svg";
+import EyeB from "../../../../../assets/images/EyeB.svg";
+import DownloadW from "../../../../../assets/images/DownloadW.svg";
+import DownloadB from "../../../../../assets/images/DownloadB.svg";
+import CommentsW from "../../../../../assets/images/CommentsW.svg";
+import CommentsB from "../../../../../assets/images/CommentsB.svg";
+import SpeakerW from "../../../../../assets/images/SpeakerW.svg";
+import { ThemeType } from "../../../../core/types/themeTypes";
 
 const HomeFeed = () => {
   const {
-    handleFileUpload,
     handleUploadMaterial,
     user,
     course,
     hasFile,
-    setHasFile,
+    getInputProps,
+    getRootProps,
+    isDragActive,
+    preview,
+    removeFiles,
+    openDialog,
+    closeDialog,
+    dialogRef,
+    time,
+    date,
+    getIcon,
+    isDark,
+    today,
+    icon,
   } = useHomeFeedLogic();
 
   return (
     <div className="home-feed">
-      <div className="home-nav">
+      <div className={`home-nav ${icon}`}>
         <div className="home-nav-section">
           <h2>{course.courseName}</h2>
           <h4>{course.courseCode}</h4>
@@ -28,31 +49,130 @@ const HomeFeed = () => {
             <button className="green--button">Request a Meeting</button>
           </div>
         ) : (
-          <div className="instructor-options">
-            <button
-              onClick={handleUploadMaterial}
-              className={hasFile ? "green--button" : "disabled--button"}
-            >
-              Add Material
-            </button>
-            <input
-              type="file"
-              name="file"
-              onChange={(e) => {
-                setHasFile(true);
-                handleFileUpload(e);
-              }}
-            />
-          </div>
+          <>
+            <div className="instructor-options">
+              <button
+                className="green--button instructor-options-btn"
+                onClick={openDialog}
+              >
+                Anncounce
+                <img src={SpeakerW} alt="" />
+              </button>
+              <button
+                className="green--button instructor-options-btn"
+                onClick={openDialog}
+              >
+                Upload
+                <img src={Upload} alt="Upload" />
+              </button>
+            </div>
+            <dialog className="dialog" ref={dialogRef}>
+              <div className="dialog-options">
+                <div className="dialog-left-section">
+                  <div className="dialog-section">
+                    <label htmlFor="types">File Type:</label>
+                    <select name="types" className="dropdown-menu">
+                      <option value={"Quiz"}>Quiz</option>
+                      <option value={"Assignment"}>Assignment</option>
+                      <option value={"Exam"}>Exam</option>
+                    </select>
+                  </div>
+                  <div className="dialog-section">
+                    <label htmlFor="dateTime">Due Date:</label>
+                    <input
+                      // className={isDark ? "date-picker--dark" : ""}
+                      type="datetime-local"
+                      name="dateTime"
+                      min={today}
+                      defaultValue={today}
+                    />
+                  </div>
+                </div>
+                <div className="dialog-right-section">
+                  <div className="drop-zone" {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    {isDragActive ? (
+                      <p className="drop-zone-area">Drop the files here...</p>
+                    ) : (
+                      <p className="drop-zone-area ">
+                        Drag & drop some files here, or click to select files
+                      </p>
+                    )}
+                  </div>
+                  {preview ? (
+                    <div className="dialog-preview">
+                      <h6>{preview as string}</h6>
+                      <button
+                        onClick={removeFiles}
+                        className={preview ? "red--button" : "disabled--button"}
+                      >
+                        Remove Files
+                      </button>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  <div className="dialog-buttons">
+                    <button
+                      onClick={hasFile ? handleUploadMaterial : () => {}}
+                      className={hasFile ? "green--button" : "disabled--button"}
+                    >
+                      Send
+                    </button>
+                    <button onClick={closeDialog} className="red--button">
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </dialog>
+          </>
         )}
       </div>
-      {course.courseMaterial === null ? (
-        <></>
-      ) : (
-        course.courseMaterial?.map((material) => {
-          return <div className="home-feed-material">{material}</div>;
-        })
-      )}
+      <div className="home-feed-material">
+        {course.courseMaterial.length > 0 ? (
+          course.courseMaterial.map((material) => {
+            return (
+              <div className="material-cards">
+                <div key={material._id} className="material-card">
+                  <div className="material-info-section">
+                    <div className="file-type-img">
+                      <img
+                        src={getIcon(material.fileType, isDark)}
+                        alt="file-type"
+                      />
+                    </div>
+                    <section className="material-name-type">
+                      <h3>{material.fileName}</h3>
+                      <p className="file-section">{material.fileSection}</p>
+                    </section>
+                  </div>
+                  <section className="material-date-section">
+                    <p className="">{date(material.deliverTime)}</p>
+                    <p className="">{`${time(material.dueTime)}`}</p>
+                  </section>
+                </div>
+                <div className="material-card-buttons">
+                  <div className="material-card-button material-card-button--eye">
+                    <img src={isDark ? EyeW : EyeB} alt="" />
+                  </div>
+                  <div className="material-card-button material-card-button--download">
+                    <img src={isDark ? DownloadW : DownloadB} alt="" />
+                  </div>
+                  <div className="material-card-button material-card-button--commnets">
+                    <img
+                      src={isDark === ThemeType.dark ? CommentsW : CommentsB}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h1>This Course Has No Material Yet</h1>
+        )}
+      </div>
     </div>
   );
 };
--- frontend-web/src/pages/Home/Home Components/Home Feed/useHomeFeedLogic.ts
+++ frontend-web/src/pages/Home/Home Components/Home Feed/useHomeFeedLogic.ts
@@ -1,15 +1,30 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useTime } from "../../../../hooks/useTime";
+import { useDate } from "../../../../hooks/useDate";
+import { useFileIcon } from "../../../../hooks/useFileIcon";
+import { useDropzone } from "react-dropzone";
 import { RootState } from "../../../../redux/store";
 import { setCurrentCourse } from "../../../../redux/course";
 import axios from "axios";
+import { useBackgroundIcon } from "../../../../hooks/useBackgroundIcon";
 
 export const useHomeFeedLogic = () => {
   const course = useSelector((state: RootState) => state.selectedCourse.course);
+  const user = useSelector((state: RootState) => state.user.userType);
+  const isDark = useSelector((state: RootState) => state.theme.currentTheme);
 
   const { courseId } = useParams();
+
+  const [hasFile, setHasFile] = useState(false);
+  const [preview, setPreview] = useState<string | ArrayBuffer | null>();
+
   const dispatch = useDispatch();
+
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const icon = useBackgroundIcon(course.icon, isDark);
 
   const getCourse = {
     method: "GET",
@@ -24,30 +39,36 @@ export const useHomeFeedLogic = () => {
         console.log(course);
       })
       .catch((e) => console.log(e));
+  }, [courseId]);
+
+  const onDrop = useCallback((acceptedFiles: any) => {
+    const file = new FileReader();
+
+    file.onload = () => {
+      acceptedFiles.forEach((f: File) => {
+        setPreview((prev) => (prev ? prev + " / " + f.name : f.name));
+      });
+    };
+
+    setHasFile(true);
+
+    file.readAsDataURL(acceptedFiles[0]);
   }, []);
 
-  const user = useSelector((state: RootState) => state.user.userType);
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({ onDrop });
 
-  const [hasFile, setHasFile] = useState(false);
-  const [file, setFile] = useState<File | undefined>(undefined);
-
-  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement & {
-      files: FileList;
-    };
-    console.log(target.files);
-    target.files.length < 1 ? setHasFile(false) : setHasFile(true);
-    setFile(target.files[0]);
+  const removeFiles = () => {
+    acceptedFiles.length = 0;
+    setPreview(null);
+    setHasFile(false);
   };
 
   const handleUploadMaterial = () => {
-    console.log("Button clicked");
+    if (typeof acceptedFiles[0] === undefined) return;
 
-    if (file === undefined) {
-      return;
-    }
     const formData = new FormData();
-    formData.append("file", file);
+    formData.append("file", acceptedFiles[0]);
 
     const uploadMaterial = {
       method: "POST",
@@ -61,12 +82,42 @@ export const useHomeFeedLogic = () => {
       .catch((e) => console.log(e));
   };
 
+  const openDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+    }
+  };
+
+  const closeDialog = () => {
+    if (dialogRef.current) {
+      dialogRef.current.close();
+    }
+  };
+
+  const getIcon = useFileIcon;
+  const time = useTime;
+  const date = useDate;
+
+  const today = new Date().toISOString().split("T")[0];
+
   return {
-    handleFileUpload,
     handleUploadMaterial,
     user,
     course,
     hasFile,
-    setHasFile,
+    isDragActive,
+    getRootProps,
+    getInputProps,
+    preview,
+    removeFiles,
+    openDialog,
+    closeDialog,
+    dialogRef,
+    time,
+    date,
+    getIcon,
+    isDark,
+    today,
+    icon,
   };
 };
