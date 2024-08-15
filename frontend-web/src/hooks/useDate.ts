677ff5247ac54dd11d2d04c8f72f5272dbf76bf8
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-15 22:28:02 +0300

    Created the use date hook

---
+++ frontend-web/src/hooks/useDate.ts
@@ -0,0 +1,6 @@
+export const useDate = (dateString: Date | null) => {
+  if (!dateString) return;
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-CA");
+  return formattedDate;
+};
