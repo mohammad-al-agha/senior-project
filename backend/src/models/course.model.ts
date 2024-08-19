9182790ebd496d3e8b40f6ad7d287f9ea566400f
mohammad-al-agha <mohalagha11@gmail.com>
2024-08-19 23:35:14 +0300

    Edited the course model

--- backend/src/models/course.model.ts
+++ backend/src/models/course.model.ts
@@ -1,6 +1,45 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
-const courseSchema = new mongoose.Schema({
+export type CourseModelDoc = {
+  _id: Types.ObjectId;
+  courseName: string;
+  courseCode: string;
+  courseMeetingLink: string;
+  courseInstructor: Types.ObjectId;
+  courseStudents: Types.ObjectId[];
+  sessionDate: string;
+  time: string;
+  icon: string;
+  courseMaterial: CourseMaterialDoc[];
+};
+
+export interface CourseMaterialDoc {
+  id: string;
+  description: string | null;
+  fileName: string | null;
+  filePath: string | null;
+  fileType: string | null;
+  fileSection: string | null;
+  deliverTime: number;
+  dueTime: Date;
+  materialComments: any[];
+  studentAnswers: any[];
+}
+
+// export interface MaterialCommentsDoc {
+//   _id: Types.ObjectId;
+//   userId: string;
+//   message: string;
+// }
+
+// export interface StudentAnswerssDoc {
+//   _id: Types.ObjectId;
+//   studentId: Types.ObjectId;
+//   submittedFile: string;
+//   submissionTime: Date;
+// }
+
+const courseSchema = new mongoose.Schema<CourseModelDoc>({
   courseName: {
     type: String,
     required: true,
@@ -46,7 +85,15 @@ const courseSchema = new mongoose.Schema({
         fileSection: String || null,
         deliverTime: { type: Date, default: Date.now },
         dueTime: { type: Date, default: Date.now },
-        materialComments: { type: [String], default: [] },
+        materialComments: [
+          {
+            userId: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Student" || "Instructor",
+            },
+            message: String,
+          },
+        ],
         studentAnswers: [
           {
             studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
